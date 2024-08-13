75bba31d85b2bd00bed7781dcb1a5ada94f0c8ca
TakeOff-C <142121947+TakeOff-C@users.noreply.github.com>
2024-08-13 20:49:26 +0200

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -22,8 +22,8 @@ return(
      <nav class="navbar navbar-expand-lg sticky-top">
   <div class="container">
     <img src={logo} alt="" className="logo"/> 
-    <button class="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span><i class="bi bi-list"></i></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
