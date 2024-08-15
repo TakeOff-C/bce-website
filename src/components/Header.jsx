b13191d85dccedf0796a958b4634cea983d038d9
TakeOff-C <142121947+TakeOff-C@users.noreply.github.com>
2024-08-15 21:07:15 +0200

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -41,9 +41,15 @@ return(
   <Link class="dropdown-item" aria-current="page" to="/Physicaldamage">Physical damage insurance</Link>
   </ul>
 </div>
+<li class="nav-item">
+          <Link class="nav-link" to=""><i class="bi bi-sliders2-vertical"></i>My Policy</Link>
+        </li><li class="nav-item">
+          <Link class="nav-link" to=""><i class="bi bi-phone-flip"></i>Contact Us</Link>
+        </li>
         <li class="nav-item">
           <Link class="nav-link" to="#" type="button" ><i class="bi bi-question-octagon"></i>FAQ & Resources</Link>
         </li>
+        
         <li class="nav-item">
           <Link class="nav-link" to="/About"><i class="bi bi-file-person-fill"></i>About Us</Link>
         </li>
@@ -52,7 +58,6 @@ return(
         </li>
         <div className="nav-btn">
  <span><Link to="/Login"><button type="button" class="btn btn-login">Login</button></Link></span>
-  <span><button type="button" class="btn btn-quote" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop" aria-controls="offcanvasTop">Get a Quote</button></span>
  </div>
       </ul>
     </div>
@@ -63,45 +68,6 @@ return(
 
 
 
-{/* offcanvas menu */}
-{/* to trigger the canvas, check the trigger code on menu */}
-
-<div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel">
-  <div class="offcanvas-header">
-    <h5 id="offcanvasTopLabel"> <b>Get a quote right now</b></h5>
-    {/* badge start */}
-    <button type="button" class="btn btn-primary position-relative">
-  Registered
-  <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-    99+
-    <span class="visually-hidden">unread messages</span>
-  </span>
-</button>
-{/* badge end */}
-    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-  </div>
-  <div class="offcanvas-body">
-<p>Sign up and our team will contact you to provide your quote</p>
-  <div class="vertical-menu">
-  <div class="modal-body">
-       <form>
-       <input type="text" name="" id="" placeholder="User name" required/>
-        <br/><br/>
-        <input type="email" name="" id="" placeholder="E-mail" required/>
-        <br/><br/>
-        <input type="number" name="" id="" placeholder="Phone number" required/>
-        <br/><br/>
-<button className="btn btn-success" onClick={() => openNotificationWithIcon('success')}>I want to be a customer</button> 
-<br/><br/>
-       </form>
-<p>By submitting your data, you authorize us to contact you, To find out more, see our Privacy policy</p>
-<hr/>
-<i class="bi bi-shield-fill-check"><b>Your data is sage with us</b></i>   
-   </div>
-</div>
-
-  </div>
-</div>
 
 
 
